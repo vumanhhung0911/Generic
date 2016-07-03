3b64f28541cfe7918bf51ed15affc2450cf743e1
Hùng Vũ <vumanhhung0911@gmail.com>
2016-07-03 13:08:33 +0700

    mnbjkh k

--- Nutshell/Program.cs
+++ Nutshell/Program.cs
@@ -6,26 +6,27 @@ using System.Threading.Tasks;
 
 namespace Nutshell
 {
-    
+
     class Program
     {
         public interface IComparable<T>
         {
             int CompareTo(T other);
         }
-        static void Initialize<T>(T[] array)where T : new()
-        {
-            for (int i = 0; i < array.Length; i++)
-                array[i] = new T();
-        }
-        static T Max <T>(T a, T b) where T : IComparable<T>
+
+        static T Max<T>(T a, T b) where T : IComparable<T>
         {
             return a.CompareTo(b) > 0 ? a : b;
         }
         static void Main(string[] args)
         {
-            int z = Max(5,10 );
+            int z = Max(5, 10);
             string last = Max("ant", "zoo");
+
+            Console.WriteLine(z);
+            Console.WriteLine(last);
+
+            Console.Read();
         }
     }
 }
